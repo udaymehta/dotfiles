37ab99ed80903e99b14895a611356917a7b9c87e
udaymehta <udaymehta.1011@gmail.com>
2022-09-01 09:49:04 +0530

    U: Home Manager Packages

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -13,23 +13,38 @@ in
   home.homeDirectory = "/home/uday";
 
   home.packages = [
-      pkgs.unzip
-      pkgs.vlc
-      pkgs.handlr
-      pkgs.htop
-      pkgs.neofetch
-      pkgs.flatpak
-      pkgs.discord
-      pkgs.google-chrome
-      pkgs.gnomeExtensions.appindicator
-      pkgs.gnomeExtensions.dash-to-dock
-    ];
+    pkgs.unzip
+    pkgs.vlc
+    pkgs.handlr
+    pkgs.htop
+    pkgs.neofetch
+    pkgs.flatpak
+    pkgs.discord
+    pkgs.google-chrome
+    pkgs.appimage-run
+    pkgs.appimagekit
+    pkgs.nixpkgs-fmt
+    pkgs.starship
+    pkgs.libsecret
+    pkgs.gnomeExtensions.appindicator
+    pkgs.gnomeExtensions.dash-to-dock
+  ];
 
-    programs.git = {
-      enable = true;
-      userName = "udaymehta";
-      userEmail = "udaymehta.1011@gmail.com";
+  programs.git = {
+    enable = true;
+    userName = "udaymehta";
+    userEmail = "udaymehta.1011@gmail.com";
+  };
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = "eval \"$(starship init bash)\"";
+    shellAliases = {
+      ll = "ls -la";
+      ".." = "cd ..";
     };
+  };
+
 
   home.stateVersion = "22.05";
 }
