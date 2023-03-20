b0ae9376f3179a45522fe9549104ce801b5f7ab2
udaymehta <udaymehta.1011@gmail.com>
2023-03-20 20:59:54 +0530

    U: Simple Error Handling

--- install.sh
+++ install.sh
@@ -1,23 +1,66 @@
-# Fonts
-cp -r ~/dotfiles/.local/share/fonts ~/.local/share/
+#!/bin/bash
 
-# Hypr
-cp -r ~/dotfiles/hypr ~/.config/
+# Copies dotfiles from $HOME/dotfiles to $HOME/.config
+function copy_dotfiles() {
+    local dotfiles_dir="$HOME/dotfiles"
+    local config_dir="$HOME/.config"
 
-# Alacritty
-cp -r ~/dotfiles/alacritty ~/.config/
+    # Copy hypr files
+    if [[ -d "$dotfiles_dir/hypr" ]]; then
+        cp -r "$dotfiles_dir/hypr" "$config_dir/" \
+            || { echo "Error: could not copy hypr"; exit 1; }
+    else
+        echo "Warning: could not find hypr directory"
+    fi
 
-# Alacritty
-cp -r ~/dotfiles/dunst ~/.config/
+    # Copy alacritty files
+    if [[ -d "$dotfiles_dir/alacritty" ]]; then
+        cp -r "$dotfiles_dir/alacritty" "$config_dir/" \
+            || { echo "Error: could not copy alacritty"; exit 1; }
+    else
+        echo "Warning: could not find alacritty directory"
+    fi
 
-# Fontconfig
-cp -r ~/dotfiles/fontconfig ~/.config/
+    # Copy dunst files
+    if [[ -d "$dotfiles_dir/dunst" ]]; then
+        cp -r "$dotfiles_dir/dunst" "$config_dir/" \
+            || { echo "Error: could not copy dunst"; exit 1; }
+    else
+        echo "Warning: could not find dunst directory"
+    fi
 
-# Waybar
-cp -r ~/dotfiles/waybar ~/.config/
+    # Copy fontconfig files
+    if [[ -d "$dotfiles_dir/fontconfig" ]]; then
+        cp -r "$dotfiles_dir/fontconfig" "$config_dir/" \
+            || { echo "Error: could not copy fontconfig"; exit 1; }
+    else
+        echo "Warning: could not find fontconfig directory"
+    fi
 
-# Wlogout
-cp -r ~/dotfiles/wlogout ~/.config/
+    # Copy waybar files
+    if [[ -d "$dotfiles_dir/waybar" ]]; then
+        cp -r "$dotfiles_dir/waybar" "$config_dir/" \
+            || { echo "Error: could not copy waybar"; exit 1; }
+    else
+        echo "Warning: could not find waybar directory"
+    fi
 
-# Wofi
-cp -r ~/dotfiles/wofi ~/.config/
+    # Copy wlogout files
+    if [[ -d "$dotfiles_dir/wlogout" ]]; then
+        cp -r "$dotfiles_dir/wlogout" "$config_dir/" \
+            || { echo "Error: could not copy wlogout"; exit 1; }
+    else
+        echo "Warning: could not find wlogout directory"
+    fi
+
+    # Copy wofi files
+    if [[ -d "$dotfiles_dir/wofi" ]]; then
+        cp -r "$dotfiles_dir/wofi" "$config_dir/" \
+            || { echo "Error: could not copy wofi"; exit 1; }
+    else
+        echo "Warning: could not find wofi directory"
+    fi
+}
+
+# Execute the script
+copy_dotfiles
