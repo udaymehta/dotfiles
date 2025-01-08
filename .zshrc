1689f3562de63adfd1bb0b285eee8af3d1cd9960
udaymehta <udaymehta.1011@gmail.com>
2025-01-08 19:03:36 +0530

    chore: added default lsp

--- .zshrc
+++ .zshrc
@@ -3,7 +3,8 @@
 
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.local/bin:/opt/cuda/bin"
+export LD_LIBRARY_PATH="/opt/cuda/lib64:$LD_LIBRARY_PATH"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time Oh My Zsh is loaded, in which case,
--- nvim/lua/main/plugins/lsp.lua
+++ nvim/lua/main/plugins/lsp.lua
@@ -26,29 +26,13 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "lua_ls", "pyright"
+                "lua_ls", "ruff", "pylsp", "clangd"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {
                         capabilities = capabilities
                     }
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
                 end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
